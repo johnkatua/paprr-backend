40e51a8abbe489db49f86bdbc9476db2f5d94c02
john katua <johnkatua99@gmail.com>
2022-06-08 21:47:48 +0300

    added a route to delete a single paper

--- src/resources/paper/paper.router.js
+++ src/resources/paper/paper.router.js
@@ -4,6 +4,10 @@ import controllers from './paper.controllers';
 const router = Router();
 
 router.route("/")
+  .get(controllers.getAll)
   .post(controllers.createOne)
+
+router.route("/:id")
+  .delete(controllers.removeItem)
 
 export default router;
