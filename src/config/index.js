7bc1dcb5e9fea389a7781134daa339bfa3fb3074
john katua <johnkatua99@gmail.com>
2022-06-16 17:30:20 +0300

    added a port to be dynamically set

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const baseConfig = {
-  port: 3000,
+  port: process.env.PORT,
   dbUrl: process.env.MONGO_URI
-  // dbUrl: "mongodb://localhost:27017/pappr",
 };
